5d4624061e446f7e4714ee367fa74f6f10205a0f
FULLMETAL555 <parganihaj@gmail.com>
2025-08-27 17:58:32 +0530

    'final_2'

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:9099/api" || process.env.REACT_APP_API_BASE_URL, // backend URL
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:9099/api",
   headers: { "Content-Type": "application/json" },
 });
 
