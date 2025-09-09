9f06684bbcce93279ad954c4f407ac8f7d0c6f61
FULLMETAL555 <parganihaj@gmail.com>
2025-09-10 00:50:34 +0530

    'final_2_frdocfix__BRUTEAPI'

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+// process.env.REACT_APP_API_BASE_URL || "/api",
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "/api",
+  baseURL: "/api",
   headers: { "Content-Type": "application/json" },
 });
 
